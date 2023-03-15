9e8010d89d3b214de2924f968bbba1178d4ebc78
Anthony Hebert <anhebert@c2r1p3.42quebec.com>
2023-03-15 14:14:56 -0400

    15marchfirstcommit

--- Users.cpp
+++ Users.cpp
@@ -1,11 +1,44 @@
 #include "Users.hpp"
 
-Users::Users(std::string Nn, std::string Un): Nickname(Nn), Username(Un)
-{
+Users::Users(){}
 
+Users::~Users(){}
+
+int	Users::getFd()
+{
+	return fd;
+}
+std::string	&Users::getNickname()
+{
+	return Nickname;
+}
+std::string	&Users::getUsername()
+{
+	return Username;
 }
 
-Users::~Users()
+void	Users::setFd(int fdesc)
 {
+	fd = fdesc;
+}
+void	Users::setNickname(std::string &name)
+{
+	Nickname = name;
+}
+void	Users::setUserName(std::string &name)
+{
+	Username = name;
+}
 
+void	Users::addChan(Channels &Chan)
+{
+	if (Chans.find(Chan.getname()) == Chans.end())
+		Chans.insert(std::pair(Chan.getname(), Chan));
+}
+
+bool	Users::isInChan(std::string &Chan)
+{
+	if (Chans.find(Chan.getname()) == Chans.end())
+		return false;
+	return true;
 }
