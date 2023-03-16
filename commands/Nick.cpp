e420e8695dc685e159091c7a9ce2816b80eb5234
Anthony Hebert <anhebert@c2r1p3.42quebec.com>
2023-03-16 10:09:56 -0400

    secondof16march

--- commands/Nick.cpp
+++ commands/Nick.cpp
@@ -2,17 +2,23 @@
 
 int     Servers::Nick(Users &user, Parser &parser)
 {
-    (void)user;
-    (void)parser;
-/*     std::set<std::string>::iterator it;
-    it = this->Nickname_list.begin();
-
-    if (usersMap.find(parser.getArgs()[0]) == usersMap.end())
+    if (parser.getArgs().empty())
     {
-            send();
+            send(user.getFd(), "431", 3, 0);
             return (1);
     }
+    if (Nickname_list.find(parser.getArgs()[0]) == Nickname_list.end())
+    {
+            send(user.getFd(), "433", 3, 0);
+            return (1);
+    }
+    if (parser.getArgs()[0].length() > 9 || parser.getArgs()[0] || parser.getArgs()[0] ||
+        parser.getArgs()[0] || parser.getArgs()[0] || parser.getArgs()[0])
+        
+    if (Nickname_list.find(user.getNickname()) != Nickname_list.end())
+        Nickname_list.erase(user.getNickname());
     user.setNickname(parser.getArgs()[0]);
-    send(user.getFd(), , 0); */
+    Nickname_list.insert(parser.getArgs()[0]);
+    send(user.getFd(), "001", 3, 0);
     return (0);
 }
