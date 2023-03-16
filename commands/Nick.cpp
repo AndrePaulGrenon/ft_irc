d7d8bf49b0f4972692be8eb11cb94090db038ca0
Anthony Hebert <anhebert@c2r1p3.42quebec.com>
2023-03-16 11:42:42 -0400

    Nick almost done

--- commands/Nick.cpp
+++ commands/Nick.cpp
@@ -4,9 +4,12 @@ bool    ft_isgoodchar(std::string nick)
 {
     for(std::string::iterator it = nick.begin(); it != nick.end(); it++)
     {
+        std::cout << "sauce " << *it << std::endl;
+        if (it == nick.end())
+            return true;
         if (!isalnum(*it) && *it != '-' && *it != '[' &&
-            *it != ']' && *it != '_' && *it != '{' &&
-            *it != '}' && *it != '\\' && *it != '`' && *it != '|')
+            *it != ']' && *it != '_' && *it != '{' && *it != '\r' &&
+            *it != '}' && *it != '\\' && *it != '`' && *it != '|' && *it != '\n')
             return false;
     }
     return true;
@@ -16,23 +19,26 @@ int     Servers::Nick(Users &user, Parser &parser)
 {
     if (parser.getArgs().empty())
     {
-            send(user.getFd(), "431", 3, 0);
+            send(user.getFd(), parser.SendReply("431", parser.getArgs()[0], "Nickname empty, please enter valid Nickname\n"), 512, 0);
+            close_connection = true;
             return (1);
     }
     if (parser.getArgs()[0].length() > 9 || !ft_isgoodchar(parser.getArgs()[0]))
     {
-            send(user.getFd(), "432", 3, 0);
+            send(user.getFd(), parser.SendReply("432", parser.getArgs()[0], "Nickname invalid, please enter valid Nickname\n"), 512, 0);
+            close_connection = true;
             return (1);
     }
-    if (Nickname_list.find(parser.getArgs()[0]) == Nickname_list.end())
+    if (Nickname_list.find(parser.getArgs()[0]) != Nickname_list.end())
     {
-            send(user.getFd(), "433", 3, 0);
+            send(user.getFd(), parser.SendReply("433", parser.getArgs()[0], "Nickname already in use, please enter another valid Nickname\n"), 512, 0);
+            close_connection = true;
             return (1);
     }
     if (Nickname_list.find(user.getNickname()) != Nickname_list.end())
         Nickname_list.erase(user.getNickname());
     user.setNickname(parser.getArgs()[0]);
     Nickname_list.insert(parser.getArgs()[0]);
-    send(user.getFd(), "001", 3, 0);
+    send(user.getFd(), parser.SendReply("001", parser.getArgs()[0], "Nickname valid\n"), 512, 0);
     return (0);
 }
