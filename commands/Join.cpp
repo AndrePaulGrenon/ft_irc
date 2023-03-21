c212f0dc101a21f82354d5ef38dbb2d30e8b1ef3
SuperTardig <benjaminperron150@gmail.com>
2023-03-21 14:24:17 -0400

    forgot prints

--- commands/Join.cpp
+++ commands/Join.cpp
@@ -1,18 +1,12 @@
 #include "../Servers.hpp"
 
 int     Servers::Join(Users &user, Parser &parser){
-	std::cout << "ok" << std::endl;
 	if (this->Chans.empty())
 	{
-	std::cout << "ok" << std::endl;
 		Channels test("test");
-	std::cout << "ok" << std::endl;
 		this->Chans.insert(std::pair<std::string, Channels>("test", test));
-	std::cout << "ok" << std::endl;
 		this->Chans.at("test").setOp(user.getNickname(), true);
-	std::cout << "ok" << std::endl;
 		this->Chans.at("test").addUser(user);
-		std::cout << "channel created" << std::endl;
 	}
 	else{
 		this->Chans.at("test").addUser(user);
