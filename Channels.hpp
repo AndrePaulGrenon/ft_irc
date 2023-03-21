6e604bae2e06192e5799cda40a2e2d9dd7a1c0fa
SuperTardig <benjaminperron150@gmail.com>
2023-03-21 14:20:22 -0400

    made a test channel

--- Channels.hpp
+++ Channels.hpp
@@ -4,8 +4,9 @@
 #pragma once
 #include "Users.hpp"
 #include <iostream>
-#include <map>
 #include <string>
+#include <utility>
+#include <vector>
 
 #define P 0;
 #define S 1;
@@ -35,12 +36,15 @@ public:
   const int &getLimit() const;
   const bool &getOp(const string &user) const;
 
+  void  addUser(const Users &user);
+
 private:
   int _limit;
   string _pass;
   string _name;
   string _topic;
   bool _flags[6];
+  std::vector<Users>  _users;
   std::map<string, bool> _operators;
 };
 
