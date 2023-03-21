9ac87adc91720f3e4792afc9f4b4350a4aecd580
Anthony Hebert <anhebert@c2r1p3.42quebec.com>
2023-03-21 13:31:38 -0400

    PrivmsgInProgress

--- commands/Privmsg.cpp
+++ commands/Privmsg.cpp
@@ -5,13 +5,13 @@ int	Servers::Privmsg(Users &user, Parser &parser)
 	if (parser.getMessage().size() == 0)
 	{
 				send(user.getFd(), parser.SendReply("412", "", "No message to send!\n"), parser.getReply().size(), 0);
-				_close_connection = true;
+				//_close_connection = true;
 				return (1);
 	}
 	if (parser.getArgs()[0].size() == 0)
 	{
 				send(user.getFd(), parser.SendReply("411", "", "No recipient has been given!\n"), parser.getReply().size(), 0);
-				_close_connection = true;
+				//_close_connection = true;
 				return (1);
 	}
 	/* if (parser.getArgs()[0][0] == '#')
@@ -35,16 +35,17 @@ int	Servers::Privmsg(Users &user, Parser &parser)
 	} */
 	else
 	{
-		for (size_t i = 0; i < (parser.getArgs().size()); i++)
+		std::vector<std::string> ulist;
+		for (size_t i = 0; i < (ulist.size()); i++)
 		{
-			if (userPointer.find(parser.getArgs()[i]) == userPointer.end())
+			if (userPointer.find(ulist[i]) == userPointer.end())
 			{
 				send(user.getFd(), parser.SendReply("401", parser.getArgs()[i], "User you try to communicate with doesn't exists\n"), parser.getReply().size(), 0);
-				_close_connection = true;
+				//_close_connection = true;
 				return (1);
 			}
 		}
-		for (size_t i = 0; i < (parser.getArgs().size()); i++)
+		for (size_t i = 0; i < (ulist.size()); i++)
 		{
 			send(userPointer.find(parser.getArgs()[i])->second->getFd(), parser.SendReply("", user.getNickname(), parser.getMessage()), parser.getReply().size(), 0);
 		}
