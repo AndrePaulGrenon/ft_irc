417eec0035ffce99c68b64ddb04c21017ccad688
Anthony Hebert <anhebert@c2r1p3.42quebec.com>
2023-03-22 15:00:54 -0400

    lastcoomitof22march

--- commands/Part.cpp
+++ commands/Part.cpp
@@ -12,27 +12,20 @@ int	Servers::Part(Users &user, Parser &parser)
 	{
 		std::map<std::string, Channels>::iterator	it = Chans.find(clist[i]);
 		if (it == Chans.end())
-		{
 			send(user.getFd(), parser.SendReply("403", parser.getArgs()[i], "non-existant Channel"), parser.getReply().size(), 0);
-			//_close_connection = true;
-			//return (1);
-		}
 		else if ((it->second.getFlag(4) == true && user.getChannels().find(it->second.getName()) != user.getChannels().end())) /*|| If moderate and banned*/
-		{
 			send(user.getFd(), parser.SendReply("442", parser.getArgs()[0], "You don't have access to the channel"), parser.getReply().size(), 0);
-			//_close_connection = true;
-			//return (1);
-		}
 		else
 		{
 			user.removeChannel(it->second.getName());
-			for (std::vector<Users>::iterator vec_it = it->second.getUsers().begin(); vec_it != it->second.getUsers().end(); vec_it++)
+			//it->second.removeUser(user); //utiliser nouvelle fonction delete user de a-p
+/* 			for (std::vector<Users>::iterator vec_it = it->second.getUsers().begin(); vec_it != it->second.getUsers().end(); vec_it++)
 			{
 				if (vec_it->getNickname() == user.getNickname())
 				{
 					it->second.getUsers().erase(vec_it);
 				}
-			}
+			} */
 		}
 	}
 	return (0);
